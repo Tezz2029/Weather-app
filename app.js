f621b4d054a70168508ca364089cd15bfd69142b
Tejas Lanjekar <tejaslanjekar2000@gmail.com>
2021-03-10 19:22:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,119 @@
+const key = "8cd80d49e5c0dcb0f5b9437939f63e0c";
+
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+function success(pos){
+    var latitude = pos.coords.latitude;
+    var longitude = pos.coords.longitude;
+    weather.fetchCountryId(latitude, longitude);
+};
+
+function error(err){
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+//geoLocation ends
+let weather = 
+{
+    "apikey": key,
+    fetchCountryId: (latitude, longitude) =>{
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`).then((response)=> response.json())
+        .then((data)=>{
+            const {name} = data;
+            const {country} = data.sys;
+            weather.fetchOnecallWeather(latitude,longitude,country,name);
+        });
+        
+    },
+
+    fetchLatLon:(city)=> {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`)
+        .then((response)=> response.json())
+        .then((data)=>{
+            const {lat,lon} = data.coord;
+            const {country} = data.sys;
+            weather.displayWeather(lat,lon,country,city);
+        });
+
+    },
+
+    fetchOnecallWeather:function(latitude, longitude, country, city)
+    {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`)
+        .then((response)=> response.json()).then((data)=>this.displayWeather(data,country,city));
+
+    },
+
+    displayWeather:function(data,country,city){
+        const {icon,description} = data.current.weather[0];
+        const {humidity,temp,wind_speed} = data.current;
+        const {morn,day,eve,night} = data.daily[0].temp;
+        document.querySelector(".location").innerText =`${city},${country}`;
+        document.querySelector(".weather-icon").src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+        document.querySelector(".weather-temp").innerHTML = `${temp}`;
+        document.querySelector(".weather-desc").innerHTML = `${description.toUpperCase()}`;
+        document.querySelector("#h-value").innerHTML = `${humidity}`;
+        document.querySelector("#h-value").innerHTML = `${wind_speed}`;
+        document.body.style.backgroundImage = `url('https://source.unsplash.com/1920x1080/?${city}')`;
+        document.querySelector("#temp1").innerHTML = `${morn}`;
+        document.querySelector("#temp2").innerHTML = `${day}`;
+        document.querySelector("#temp3").innerHTML = `${eve}`;
+        document.querySelector("#temp4").innerHTML = `${night}`;
+    },
+
+    search:function()
+    {
+        this.fetchLatLon(document.querySelector(".search").value);
+    },
+
+    LocalDay:function()
+    {
+        var d = new Date();
+        var weekday = new Array(7);
+        weekday[0] = "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+        var day = weekday[d.getDay()]
+        document.querySelector(".day-dayname").innerHTML = day;
+        var dd = d.getDate();
+        var mm = new Array(12);
+        mm[1] = "January";
+        mm[2] = "February";
+        mm[3] = "March";
+        mm[4] = "April";
+        mm[5] = "May";
+        mm[6] = "June";
+        mm[7] = "July";
+        mm[8] = "August";
+        mm[9] = "September";
+        mm[10] = "October";
+        mm[11] = "November";
+        mm[12] = "December";
+        var month = mm[d.getMonth()+1];
+        var yyyy = d.getFullYear();
+        document.querySelector(".day-day").innerHTML = dd+" "+month+", "+yyyy;
+
+
+    }
+};
+//when you press enter
+document.querySelector(".location-button").addEventListener("click", function() 
+{
+    weather.search();
+});
+//Event Listner for enter and click
+document.querySelector(".search").addEventListener("keypress", function(random) 
+{
+    if (random.key == "Enter") {
+        weather.search();
+    }
+});
